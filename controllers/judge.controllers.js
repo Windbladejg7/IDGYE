1ad5c2ec91e3879c369775472096890c09c478ac
Windblade <juliangarzon867@gmail.com>
2025-07-09 07:51:17 -0500

    Obtener submission a partir de token
    Agregar consola

--- controllers/judge.controllers.js
+++ controllers/judge.controllers.js
@@ -1,7 +1,6 @@
 
 export async function enviarCodigo(req, res){
     const {code} = req.body;
-    console.log(code);
     const response = await fetch("https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=false&wait=false", {
         method:"POST",
         headers:{
@@ -14,7 +13,18 @@ export async function enviarCodigo(req, res){
           source_code:code 
         })
     });
-    const token = await response.json();
-    console.log(token);
-    res.json(token);
+    const {token} = await response.json();
+    const result = await obtenerSubmission(token);
+    res.json(result);
+}
+
+async function obtenerSubmission(token){
+    const response = await fetch(`https://ce.judge0.com/submissions/${token}?base64_encoded=false&fields=stdout,stderr,status_id,language_id`, {
+        headers:{
+            "X-RapidApi-Host":"judge0-ce.p.rapidapi.com",
+            "X-RapidApi-Key":"13abcef35amsh45dad28498a63dfp140007jsnac3604e3c55f"
+        }
+    });
+    const datos = await response.json();
+    return datos;
 }
---
+++ db/createTables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE PRUEBA(
+    id_prueba SERIAL PRIMARY KEY,
+    titulo TEXT UNIQUE NOT NULL,
+    descripcion TEXT NOT NULL,
+    fecha_creacion DATE DEFAULT CURRENT_DATE,
+    fecha_max DATE NOT NULL,
+    inicia TIME DEFAULT LOCALTIME(0),
+    hora_max TIME NOT NULL
+);
+
+CREATE TABLE DOCENTE(
+    id_docente SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE CURSO(
+    id_curso SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    id_docente INT,
+    CONSTRAINT fk_docente FOREIGN KEY(id_docente) REFERENCES DOCENTE(id_docente)
+);
+
+CREATE TABLE PRUEBA_CURSO(
+    id_curso INT,
+    id_prueba INT,
+    CONSTRAINT fk_curso FOREIGN KEY(id_curso) REFERENCES CURSO(id_curso),
+    CONSTRAINT fk_prueba FOREIGN KEY(id_prueba) REFERENCES PRUEBA(id_prueba),
+    CONSTRAINT pk_prueba_curso PRIMARY KEY(id_curso, id_prueba)
+);
+
+CREATE TABLE ESTUDIANTE(
+    id_estudiante SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    id_curso INT,
+    CONSTRAINT fk_curso FOREIGN KEY(id_curso) REFERENCES CURSO(id_curso)
+);
+
+CREATE TABLE ENTREGA(
+    id_entrega SERIAL PRIMARY KEY,
+    arbol_archivos JSONB NOT NULL,
+    calificacion NUMERIC(4,2),
+    fecha_entrega DATE DEFAULT CURRENT_DATE,
+    hora_entrega TIME DEFAULT LOCALTIME(0),
+
+    id_prueba INT,
+    id_estudiante INT,
+    CONSTRAINT fk_prueba FOREIGN KEY(id_prueba) REFERENCES PRUEBA(id_prueba),
+    CONSTRAINT fk_estudiante FOREIGN KEY(id_estudiante) REFERENCES ESTUDIANTE(id_estudiante)
+);
